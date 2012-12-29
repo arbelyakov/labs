57e468c78093a084714a5c235dbfe342dc5eb68f
tema <belyakov@gmail.com>
2012-12-29 18:09:48 +0400

    refactoring: check code style

--- n2.c
+++ n2.c
@@ -5,24 +5,23 @@
 int main() {
   int  *mas, a = 0, d, x, p, i;
   
-  mas = (int *)malloc(a*sizeof(int));
+  mas = (int*) malloc(a * sizeof(int));
 
   printf("\n");
 
   for (i = 0; ; i++) {
     a++;
-    mas = (int *)realloc(mas, a*sizeof(int));
-    scanf ("%d", &mas[i]);
-    if (mas [i] == 0) {
+    mas = (int*) realloc(mas, a * sizeof(int));
+    scanf("%d", &mas[i]);
+    if (mas[i] == 0) {
       break;
     }
   }
 
   printf("\n");
 
-  for(x = 1; x < a; x++) {
-    for (i = 0; i <= a - x; i++)
-    {
+  for (x = 1; x < a; x++) {
+    for (i = 0; i <= a - x; i++) {
       if (mas[i] > mas[i + 1]) {
         p = mas[i];
         mas[i] = mas[i + 1];
@@ -31,14 +30,14 @@ int main() {
     }
   }
   
-  for (i = 0 ; i < a; i++) {
-    printf ("%d\n", mas[i]);
+  for (i = 0; i < a; i++) {
+    printf("%d\n", mas[i]);
   }
 
   printf("\n");
 
-  for (i = 0 ; i < a; i++) {
-      printf ("%d\n", mas[a - 1 - i]);
+  for (i = 0; i < a; i++) {
+      printf("%d\n", mas[a - 1 - i]);
   }
 
   free(mas);
